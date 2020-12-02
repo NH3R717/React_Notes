1ff12b18380d7aac92d2a55791e8bb62d6cd6e5e
“Tommy” <nh3r717@gmail.com>
2020-12-02 14:03:40 -0500

    add – action comments

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -1,17 +1,22 @@
-export const CREATE_TODO = 'CREATE_TODO';
-export const createTodo = text => ({
-    type: CREATE_TODO,
-    payload: { text },
+// ! every action is json with a type property and payload
+
+// ! actions types may be held in a single folder for larger projects (actionTypes.js)
+export const CREATE_TODO = "CREATE_TODO";
+// ! function is called "action creators"
+export const createTodo = (text) => ({
+  type: CREATE_TODO,
+  // ! contains text of new ToDo
+  payload: { text },
 });
 
-export const REMOVE_TODO = 'REMOVE_TODO';
-export const removeTodo = text => ({
-    type: REMOVE_TODO,
-    payload: { text },
+export const REMOVE_TODO = "REMOVE_TODO";
+export const removeTodo = (text) => ({
+  type: REMOVE_TODO,
+  payload: { text },
 });
 
-export const MARK_TODO_AS_COMPLETED = 'MARK_TODO_AS_COMPLETED';
-export const markTodoAsCompleted = text => ({
-    type: MARK_TODO_AS_COMPLETED,
-    payload: { text },
+export const MARK_TODO_AS_COMPLETED = "MARK_TODO_AS_COMPLETED";
+export const markTodoAsCompleted = (text) => ({
+  type: MARK_TODO_AS_COMPLETED,
+  payload: { text },
 });
